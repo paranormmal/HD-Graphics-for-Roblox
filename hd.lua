90ef4ad448142a39aa85233552bbad2935edbace
paranormmal <107426846+paranormmal@users.noreply.github.com>
2022-06-13 11:56:04 -0500

    HD Graphics
    hd script for roblox

--- hd.lua
+++ hd.lua
@@ -1,27 +1,7 @@
+--// Leave it in your auto execute folder
+
 repeat task.wait() until game:IsLoaded()
 task.wait(0.06)
-
-_G.Technology = Future
-_G.Ambient = 77, 77, 77
-_G.BlurSize = 1
-_G.ColorShift_Top = 214, 107, 0
-_G.ColorShift_Bottom = 0, 0, 0
-_G.ColorCorrectionBrightness = 0.03
-_G.ColorCorrectionContrast = 0.1
-_G.ColorCorrectionSaturation = 0.80
-_G.ColorCorrectionTintColor = Color3.fromRGB(244,244,244)
-_G.OutdoorAmbient = 70, 70, 70
-_G.SunRaysIntensity = 0.5
-_G.SunRaysSpread = 2
-_G.Brightness = 1.3
-_G.GlobalShadows = true
-_G.ShadowSoftness = 100
-_G.GeographicLatitude = 350
-_G.TimeOfDay = 7.5
-_G.ExposureCompensation = 0.03
-_G.EnvironmentSpecularScale = 0.125
-_G.EnvironmentDiffuseScale = 0.154
-
 
 
 _,i = pcall(function()
